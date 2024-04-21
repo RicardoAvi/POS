6066eddcd1e0a3f0864d7e8dc33f44f692fefa95
Mirk0312 <karimmirandadelosreyes@gmail.com>
2024-04-21 00:29:12 -0800

    Ventana-nuevo producto (incompleta)

--- POS/NuevoProducto.Designer.cs
+++ POS/NuevoProducto.Designer.cs
@@ -28,30 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
+            btn_regresarP = new Button();
+            btn_crearNP = new Button();
+            txt_comentarioNP = new TextBox();
+            txt_paginaWebNP = new TextBox();
+            txt_correoNP = new TextBox();
+            txt_telefonoNP = new TextBox();
+            txt_representanteNP = new TextBox();
+            txt_empresaNP = new TextBox();
+            label1 = new Label();
+            lbl_empresaNP = new Label();
+            lbl_comentarios = new Label();
+            lbl_representanteNP = new Label();
+            lbl_paginaNP = new Label();
+            lbl_telefonoNP = new Label();
+            lbl_correoNP = new Label();
             SuspendLayout();
             // 
-            // panel1
+            // btn_regresarP
             // 
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(984, 561);
-            panel1.TabIndex = 0;
+            btn_regresarP.BackColor = Color.FromArgb(224, 224, 224);
+            btn_regresarP.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_regresarP.Location = new Point(304, 411);
+            btn_regresarP.Name = "btn_regresarP";
+            btn_regresarP.Size = new Size(107, 38);
+            btn_regresarP.TabIndex = 32;
+            btn_regresarP.Text = "Regresar";
+            btn_regresarP.UseVisualStyleBackColor = false;
+            // 
+            // btn_crearNP
+            // 
+            btn_crearNP.BackColor = Color.Gainsboro;
+            btn_crearNP.BackgroundImageLayout = ImageLayout.None;
+            btn_crearNP.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_crearNP.Location = new Point(141, 411);
+            btn_crearNP.Name = "btn_crearNP";
+            btn_crearNP.Size = new Size(108, 38);
+            btn_crearNP.TabIndex = 31;
+            btn_crearNP.Text = "Crear Proveedor";
+            btn_crearNP.UseVisualStyleBackColor = false;
+            // 
+            // txt_comentarioNP
+            // 
+            txt_comentarioNP.Location = new Point(141, 302);
+            txt_comentarioNP.Multiline = true;
+            txt_comentarioNP.Name = "txt_comentarioNP";
+            txt_comentarioNP.Size = new Size(270, 78);
+            txt_comentarioNP.TabIndex = 30;
+            // 
+            // txt_paginaWebNP
+            // 
+            txt_paginaWebNP.Location = new Point(141, 227);
+            txt_paginaWebNP.Name = "txt_paginaWebNP";
+            txt_paginaWebNP.Size = new Size(270, 23);
+            txt_paginaWebNP.TabIndex = 29;
+            // 
+            // txt_correoNP
+            // 
+            txt_correoNP.Location = new Point(141, 185);
+            txt_correoNP.Name = "txt_correoNP";
+            txt_correoNP.Size = new Size(270, 23);
+            txt_correoNP.TabIndex = 28;
+            // 
+            // txt_telefonoNP
+            // 
+            txt_telefonoNP.Location = new Point(141, 142);
+            txt_telefonoNP.Name = "txt_telefonoNP";
+            txt_telefonoNP.Size = new Size(270, 23);
+            txt_telefonoNP.TabIndex = 27;
+            // 
+            // txt_representanteNP
+            // 
+            txt_representanteNP.Location = new Point(188, 106);
+            txt_representanteNP.Name = "txt_representanteNP";
+            txt_representanteNP.Size = new Size(270, 23);
+            txt_representanteNP.TabIndex = 26;
+            // 
+            // txt_empresaNP
+            // 
+            txt_empresaNP.Location = new Point(188, 67);
+            txt_empresaNP.Name = "txt_empresaNP";
+            txt_empresaNP.Size = new Size(270, 23);
+            txt_empresaNP.TabIndex = 25;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(199, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 20);
+            label1.TabIndex = 24;
+            label1.Text = "Nuevo Producto";
+            // 
+            // lbl_empresaNP
+            // 
+            lbl_empresaNP.AutoSize = true;
+            lbl_empresaNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            lbl_empresaNP.ForeColor = Color.Black;
+            lbl_empresaNP.Location = new Point(39, 68);
+            lbl_empresaNP.Name = "lbl_empresaNP";
+            lbl_empresaNP.Size = new Size(143, 17);
+            lbl_empresaNP.TabIndex = 18;
+            lbl_empresaNP.Text = "Nombre del Producto:";
+            // 
+            // lbl_comentarios
+            // 
+            lbl_comentarios.AutoSize = true;
+            lbl_comentarios.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_comentarios.Location = new Point(141, 282);
+            lbl_comentarios.Name = "lbl_comentarios";
+            lbl_comentarios.Size = new Size(88, 17);
+            lbl_comentarios.TabIndex = 23;
+            lbl_comentarios.Text = "Comentarios:";
+            // 
+            // lbl_representanteNP
+            // 
+            lbl_representanteNP.AutoSize = true;
+            lbl_representanteNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            lbl_representanteNP.ForeColor = Color.Black;
+            lbl_representanteNP.Location = new Point(52, 107);
+            lbl_representanteNP.Name = "lbl_representanteNP";
+            lbl_representanteNP.Size = new Size(99, 17);
+            lbl_representanteNP.TabIndex = 19;
+            lbl_representanteNP.Text = "Representante:";
+            // 
+            // lbl_paginaNP
+            // 
+            lbl_paginaNP.AutoSize = true;
+            lbl_paginaNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            lbl_paginaNP.ForeColor = Color.Black;
+            lbl_paginaNP.Location = new Point(39, 233);
+            lbl_paginaNP.Name = "lbl_paginaNP";
+            lbl_paginaNP.Size = new Size(84, 17);
+            lbl_paginaNP.TabIndex = 22;
+            lbl_paginaNP.Text = "Pagina Web:";
+            // 
+            // lbl_telefonoNP
+            // 
+            lbl_telefonoNP.AutoSize = true;
+            lbl_telefonoNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            lbl_telefonoNP.ForeColor = Color.Black;
+            lbl_telefonoNP.Location = new Point(39, 148);
+            lbl_telefonoNP.Name = "lbl_telefonoNP";
+            lbl_telefonoNP.Size = new Size(62, 17);
+            lbl_telefonoNP.TabIndex = 20;
+            lbl_telefonoNP.Text = "Telefono:";
+            // 
+            // lbl_correoNP
+            // 
+            lbl_correoNP.AutoSize = true;
+            lbl_correoNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            lbl_correoNP.ForeColor = Color.Black;
+            lbl_correoNP.Location = new Point(39, 191);
+            lbl_correoNP.Name = "lbl_correoNP";
+            lbl_correoNP.Size = new Size(52, 17);
+            lbl_correoNP.TabIndex = 21;
+            lbl_correoNP.Text = "Correo:";
             // 
             // NuevoProducto
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 561);
-            Controls.Add(panel1);
+            Controls.Add(btn_regresarP);
+            Controls.Add(btn_crearNP);
+            Controls.Add(txt_comentarioNP);
+            Controls.Add(txt_paginaWebNP);
+            Controls.Add(txt_correoNP);
+            Controls.Add(txt_telefonoNP);
+            Controls.Add(txt_representanteNP);
+            Controls.Add(txt_empresaNP);
+            Controls.Add(label1);
+            Controls.Add(lbl_empresaNP);
+            Controls.Add(lbl_comentarios);
+            Controls.Add(lbl_representanteNP);
+            Controls.Add(lbl_paginaNP);
+            Controls.Add(lbl_telefonoNP);
+            Controls.Add(lbl_correoNP);
             Name = "NuevoProducto";
             Text = "NuevoProducto";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Panel panel1;
+        private Button btn_regresarP;
+        private Button btn_crearNP;
+        private TextBox txt_comentarioNP;
+        private TextBox txt_paginaWebNP;
+        private TextBox txt_correoNP;
+        private TextBox txt_telefonoNP;
+        private TextBox txt_representanteNP;
+        private TextBox txt_empresaNP;
+        private Label label1;
+        private Label lbl_empresaNP;
+        private Label lbl_comentarios;
+        private Label lbl_representanteNP;
+        private Label lbl_paginaNP;
+        private Label lbl_telefonoNP;
+        private Label lbl_correoNP;
     }
 }
