0763b63b3b3537ea9a1efd42c1b2538e55a2ee05
Mirk0312 <karimmirandadelosreyes@gmail.com>
2024-04-20 17:58:13 -0800

    Ventana proveedores

--- POS/NuevoProveedor.Designer.cs
+++ POS/NuevoProveedor.Designer.cs
@@ -28,38 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lbl_nombreNP = new Label();
+            lbl_empresaNP = new Label();
             lbl_representanteNP = new Label();
             lbl_telefonoNP = new Label();
             lbl_correoNP = new Label();
             lbl_paginaNP = new Label();
             lbl_comentarios = new Label();
             panel1 = new Panel();
-            label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            panel4 = new Panel();
-            panel2 = new Panel();
-            btn_crearNP = new Button();
-            btn_regresarP = new Button();
             panel3 = new Panel();
+            btn_regresarP = new Button();
+            btn_crearNP = new Button();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            txt_comentarioNP = new TextBox();
+            txt_paginaWebNP = new TextBox();
+            txt_correoNP = new TextBox();
+            txt_telefonoNP = new TextBox();
+            txt_representanteNP = new TextBox();
+            txt_empresaNP = new TextBox();
+            label1 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // lbl_nombreNP
+            // lbl_empresaNP
             // 
-            lbl_nombreNP.AutoSize = true;
-            lbl_nombreNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
-            lbl_nombreNP.ForeColor = Color.Black;
-            lbl_nombreNP.Location = new Point(187, 93);
-            lbl_nombreNP.Name = "lbl_nombreNP";
-            lbl_nombreNP.Size = new Size(61, 17);
-            lbl_nombreNP.TabIndex = 0;
-            lbl_nombreNP.Text = "Nombre:";
+            lbl_empresaNP.AutoSize = true;
+            lbl_empresaNP.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            lbl_empresaNP.ForeColor = Color.Black;
+            lbl_empresaNP.Location = new Point(187, 93);
+            lbl_empresaNP.Name = "lbl_empresaNP";
+            lbl_empresaNP.Size = new Size(63, 17);
+            lbl_empresaNP.TabIndex = 0;
+            lbl_empresaNP.Text = "Empresa:";
             // 
             // lbl_representanteNP
             // 
@@ -123,14 +123,14 @@
             panel1.Controls.Add(btn_crearNP);
             panel1.Controls.Add(panel2);
             panel1.Controls.Add(panel4);
-            panel1.Controls.Add(textBox6);
-            panel1.Controls.Add(textBox5);
-            panel1.Controls.Add(textBox4);
-            panel1.Controls.Add(textBox3);
-            panel1.Controls.Add(textBox2);
-            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(txt_comentarioNP);
+            panel1.Controls.Add(txt_paginaWebNP);
+            panel1.Controls.Add(txt_correoNP);
+            panel1.Controls.Add(txt_telefonoNP);
+            panel1.Controls.Add(txt_representanteNP);
+            panel1.Controls.Add(txt_empresaNP);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(lbl_nombreNP);
+            panel1.Controls.Add(lbl_empresaNP);
             panel1.Controls.Add(lbl_comentarios);
             panel1.Controls.Add(lbl_representanteNP);
             panel1.Controls.Add(lbl_paginaNP);
@@ -142,77 +142,25 @@
             panel1.Size = new Size(800, 450);
             panel1.TabIndex = 6;
             // 
-            // label1
+            // panel3
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(351, 52);
-            label1.Name = "label1";
-            label1.Size = new Size(131, 20);
-            label1.TabIndex = 6;
-            label1.Text = "Nuevo Proveedor";
+            panel3.BackColor = Color.Teal;
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(93, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(614, 40);
+            panel3.TabIndex = 18;
             // 
-            // textBox1
+            // btn_regresarP
             // 
-            textBox1.Location = new Point(289, 87);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(270, 23);
-            textBox1.TabIndex = 7;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(289, 126);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(270, 23);
-            textBox2.TabIndex = 8;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(289, 167);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(270, 23);
-            textBox3.TabIndex = 9;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(289, 210);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(270, 23);
-            textBox4.TabIndex = 10;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(289, 252);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(270, 23);
-            textBox5.TabIndex = 11;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(289, 316);
-            textBox6.Multiline = true;
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(270, 78);
-            textBox6.TabIndex = 12;
-            // 
-            // panel4
-            // 
-            panel4.BackColor = Color.Teal;
-            panel4.Dock = DockStyle.Left;
-            panel4.Location = new Point(0, 0);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(93, 450);
-            panel4.TabIndex = 14;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.Teal;
-            panel2.Dock = DockStyle.Right;
-            panel2.ImeMode = ImeMode.Off;
-            panel2.Location = new Point(707, 0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(93, 450);
-            panel2.TabIndex = 15;
+            btn_regresarP.BackColor = Color.Silver;
+            btn_regresarP.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_regresarP.Location = new Point(429, 400);
+            btn_regresarP.Name = "btn_regresarP";
+            btn_regresarP.Size = new Size(107, 38);
+            btn_regresarP.TabIndex = 17;
+            btn_regresarP.Text = "Regresar";
+            btn_regresarP.UseVisualStyleBackColor = false;
             // 
             // btn_crearNP
             // 
@@ -226,25 +174,77 @@
             btn_crearNP.Text = "Crear Proveedor";
             btn_crearNP.UseVisualStyleBackColor = false;
             // 
-            // btn_regresarP
+            // panel2
             // 
-            btn_regresarP.BackColor = Color.Silver;
-            btn_regresarP.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_regresarP.Location = new Point(429, 400);
-            btn_regresarP.Name = "btn_regresarP";
-            btn_regresarP.Size = new Size(107, 38);
-            btn_regresarP.TabIndex = 17;
-            btn_regresarP.Text = "Regresar";
-            btn_regresarP.UseVisualStyleBackColor = false;
+            panel2.BackColor = Color.Teal;
+            panel2.Dock = DockStyle.Right;
+            panel2.ImeMode = ImeMode.Off;
+            panel2.Location = new Point(707, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(93, 450);
+            panel2.TabIndex = 15;
             // 
-            // panel3
+            // panel4
             // 
-            panel3.BackColor = Color.Teal;
-            panel3.Dock = DockStyle.Top;
-            panel3.Location = new Point(93, 0);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(614, 40);
-            panel3.TabIndex = 18;
+            panel4.BackColor = Color.Teal;
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(93, 450);
+            panel4.TabIndex = 14;
+            // 
+            // txt_comentarioNP
+            // 
+            txt_comentarioNP.Location = new Point(289, 316);
+            txt_comentarioNP.Multiline = true;
+            txt_comentarioNP.Name = "txt_comentarioNP";
+            txt_comentarioNP.Size = new Size(270, 78);
+            txt_comentarioNP.TabIndex = 12;
+            // 
+            // txt_paginaWebNP
+            // 
+            txt_paginaWebNP.Location = new Point(289, 252);
+            txt_paginaWebNP.Name = "txt_paginaWebNP";
+            txt_paginaWebNP.Size = new Size(270, 23);
+            txt_paginaWebNP.TabIndex = 11;
+            // 
+            // txt_correoNP
+            // 
+            txt_correoNP.Location = new Point(289, 210);
+            txt_correoNP.Name = "txt_correoNP";
+            txt_correoNP.Size = new Size(270, 23);
+            txt_correoNP.TabIndex = 10;
+            // 
+            // txt_telefonoNP
+            // 
+            txt_telefonoNP.Location = new Point(289, 167);
+            txt_telefonoNP.Name = "txt_telefonoNP";
+            txt_telefonoNP.Size = new Size(270, 23);
+            txt_telefonoNP.TabIndex = 9;
+            // 
+            // txt_representanteNP
+            // 
+            txt_representanteNP.Location = new Point(289, 126);
+            txt_representanteNP.Name = "txt_representanteNP";
+            txt_representanteNP.Size = new Size(270, 23);
+            txt_representanteNP.TabIndex = 8;
+            // 
+            // txt_empresaNP
+            // 
+            txt_empresaNP.Location = new Point(289, 87);
+            txt_empresaNP.Name = "txt_empresaNP";
+            txt_empresaNP.Size = new Size(270, 23);
+            txt_empresaNP.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(351, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Nuevo Proveedor";
             // 
             // NuevoProveedor
             // 
@@ -261,19 +261,19 @@
 
         #endregion
 
-        private Label lbl_nombreNP;
+        private Label lbl_empresaNP;
         private Label lbl_representanteNP;
         private Label lbl_telefonoNP;
         private Label lbl_correoNP;
         private Label lbl_paginaNP;
         private Label lbl_comentarios;
         private Panel panel1;
-        private TextBox textBox6;
-        private TextBox textBox5;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox txt_comentarioNP;
+        private TextBox txt_paginaWebNP;
+        private TextBox txt_correoNP;
+        private TextBox txt_telefonoNP;
+        private TextBox txt_representanteNP;
+        private TextBox txt_empresaNP;
         private Label label1;
         private Button btn_regresarP;
         private Button btn_crearNP;
--- POS/Proveedores.Designer.cs
+++ POS/Proveedores.Designer.cs
@@ -29,15 +29,21 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            btn_nuevoProveedor = new Button();
-            btn_historialCompras = new Button();
-            label1 = new Label();
-            btn_eliminarProveedor = new Button();
-            pictureBox1 = new PictureBox();
             btn_regresarPC = new Button();
+            pictureBox1 = new PictureBox();
+            btn_eliminarProveedor = new Button();
+            label1 = new Label();
+            btn_historialCompras = new Button();
+            btn_nuevoProveedor = new Button();
             tabControl1 = new TabControl();
-            tabla_numero = new TabPage();
-            tabla_nombreEmpresa = new TabPage();
+            tabla_empresa = new TabPage();
+            btn_editarProveedor = new Button();
+            tabla_representante = new TabPage();
+            tabla_telefono = new TabPage();
+            tabla_correo = new TabPage();
+            tabla_pagina = new TabPage();
+            tabla_comentarios = new TabPage();
+            label2 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tabControl1.SuspendLayout();
@@ -45,106 +51,172 @@
             // 
             // panel1
             // 
+            panel1.BackColor = Color.Teal;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(btn_editarProveedor);
             panel1.Controls.Add(btn_regresarPC);
+            panel1.Controls.Add(btn_historialCompras);
             panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(btn_eliminarProveedor);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(btn_historialCompras);
             panel1.Controls.Add(btn_nuevoProveedor);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(800, 85);
+            panel1.Size = new Size(800, 97);
             panel1.TabIndex = 0;
             // 
-            // btn_nuevoProveedor
+            // btn_regresarPC
             // 
-            btn_nuevoProveedor.Location = new Point(3, 52);
-            btn_nuevoProveedor.Name = "btn_nuevoProveedor";
-            btn_nuevoProveedor.Size = new Size(115, 33);
-            btn_nuevoProveedor.TabIndex = 1;
-            btn_nuevoProveedor.Text = "Nuevo Proveedor";
-            btn_nuevoProveedor.UseVisualStyleBackColor = true;
-            // 
-            // btn_historialCompras
-            // 
-            btn_historialCompras.Location = new Point(124, 52);
-            btn_historialCompras.Name = "btn_historialCompras";
-            btn_historialCompras.Size = new Size(133, 33);
-            btn_historialCompras.TabIndex = 2;
-            btn_historialCompras.Text = "Historial de compras";
-            btn_historialCompras.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(3, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(134, 30);
-            label1.TabIndex = 1;
-            label1.Text = "Proveedores";
-            // 
-            // btn_eliminarProveedor
-            // 
-            btn_eliminarProveedor.Location = new Point(263, 52);
-            btn_eliminarProveedor.Name = "btn_eliminarProveedor";
-            btn_eliminarProveedor.Size = new Size(115, 33);
-            btn_eliminarProveedor.TabIndex = 3;
-            btn_eliminarProveedor.Text = "Eliminar Proveedor";
-            btn_eliminarProveedor.UseVisualStyleBackColor = true;
+            btn_regresarPC.BackColor = Color.FromArgb(224, 224, 224);
+            btn_regresarPC.Location = new Point(579, 52);
+            btn_regresarPC.Name = "btn_regresarPC";
+            btn_regresarPC.Size = new Size(115, 33);
+            btn_regresarPC.TabIndex = 5;
+            btn_regresarPC.Text = "Regresar";
+            btn_regresarPC.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
             pictureBox1.Location = new Point(700, 0);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(100, 85);
+            pictureBox1.Size = new Size(100, 97);
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
-            // btn_regresarPC
+            // btn_eliminarProveedor
             // 
-            btn_regresarPC.Location = new Point(384, 52);
-            btn_regresarPC.Name = "btn_regresarPC";
-            btn_regresarPC.Size = new Size(115, 33);
-            btn_regresarPC.TabIndex = 5;
-            btn_regresarPC.Text = "Regresar";
-            btn_regresarPC.UseVisualStyleBackColor = true;
+            btn_eliminarProveedor.BackColor = Color.FromArgb(224, 224, 224);
+            btn_eliminarProveedor.Location = new Point(301, 52);
+            btn_eliminarProveedor.Name = "btn_eliminarProveedor";
+            btn_eliminarProveedor.Size = new Size(115, 33);
+            btn_eliminarProveedor.TabIndex = 3;
+            btn_eliminarProveedor.Text = "Eliminar Proveedor";
+            btn_eliminarProveedor.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(292, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Proveedores";
+            // 
+            // btn_historialCompras
+            // 
+            btn_historialCompras.BackColor = Color.FromArgb(224, 224, 224);
+            btn_historialCompras.Location = new Point(433, 52);
+            btn_historialCompras.Name = "btn_historialCompras";
+            btn_historialCompras.Size = new Size(133, 33);
+            btn_historialCompras.TabIndex = 2;
+            btn_historialCompras.Text = "Historial de compras";
+            btn_historialCompras.UseVisualStyleBackColor = false;
+            // 
+            // btn_nuevoProveedor
+            // 
+            btn_nuevoProveedor.BackColor = Color.FromArgb(224, 224, 224);
+            btn_nuevoProveedor.Location = new Point(12, 52);
+            btn_nuevoProveedor.Name = "btn_nuevoProveedor";
+            btn_nuevoProveedor.Size = new Size(115, 33);
+            btn_nuevoProveedor.TabIndex = 1;
+            btn_nuevoProveedor.Text = "Nuevo Proveedor";
+            btn_nuevoProveedor.UseVisualStyleBackColor = false;
             // 
             // tabControl1
             // 
-            tabControl1.Controls.Add(tabla_numero);
-            tabControl1.Controls.Add(tabla_nombreEmpresa);
-            tabControl1.Location = new Point(12, 130);
+            tabControl1.Controls.Add(tabla_empresa);
+            tabControl1.Controls.Add(tabla_representante);
+            tabControl1.Controls.Add(tabla_telefono);
+            tabControl1.Controls.Add(tabla_correo);
+            tabControl1.Controls.Add(tabla_pagina);
+            tabControl1.Controls.Add(tabla_comentarios);
+            tabControl1.Location = new Point(16, 155);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(496, 251);
+            tabControl1.Size = new Size(772, 283);
             tabControl1.TabIndex = 1;
             // 
-            // tabla_numero
+            // tabla_empresa
             // 
-            tabla_numero.Location = new Point(4, 24);
-            tabla_numero.Name = "tabla_numero";
-            tabla_numero.Padding = new Padding(3);
-            tabla_numero.Size = new Size(488, 223);
-            tabla_numero.TabIndex = 0;
-            tabla_numero.Text = "Numero";
-            tabla_numero.UseVisualStyleBackColor = true;
+            tabla_empresa.Location = new Point(4, 24);
+            tabla_empresa.Name = "tabla_empresa";
+            tabla_empresa.Padding = new Padding(3);
+            tabla_empresa.Size = new Size(764, 255);
+            tabla_empresa.TabIndex = 1;
+            tabla_empresa.Text = "Empresa";
+            tabla_empresa.UseVisualStyleBackColor = true;
             // 
-            // tabla_nombreEmpresa
+            // btn_editarProveedor
             // 
-            tabla_nombreEmpresa.Location = new Point(4, 24);
-            tabla_nombreEmpresa.Name = "tabla_nombreEmpresa";
-            tabla_nombreEmpresa.Padding = new Padding(3);
-            tabla_nombreEmpresa.Size = new Size(192, 72);
-            tabla_nombreEmpresa.TabIndex = 1;
-            tabla_nombreEmpresa.Text = "Nombre de empresa";
-            tabla_nombreEmpresa.UseVisualStyleBackColor = true;
+            btn_editarProveedor.BackColor = Color.FromArgb(224, 224, 224);
+            btn_editarProveedor.Location = new Point(147, 52);
+            btn_editarProveedor.Name = "btn_editarProveedor";
+            btn_editarProveedor.Size = new Size(133, 33);
+            btn_editarProveedor.TabIndex = 6;
+            btn_editarProveedor.Text = "Editar";
+            btn_editarProveedor.UseVisualStyleBackColor = false;
+            // 
+            // tabla_representante
+            // 
+            tabla_representante.Location = new Point(4, 24);
+            tabla_representante.Name = "tabla_representante";
+            tabla_representante.Size = new Size(764, 223);
+            tabla_representante.TabIndex = 2;
+            tabla_representante.Text = "Representante";
+            tabla_representante.UseVisualStyleBackColor = true;
+            // 
+            // tabla_telefono
+            // 
+            tabla_telefono.Location = new Point(4, 24);
+            tabla_telefono.Name = "tabla_telefono";
+            tabla_telefono.Size = new Size(764, 223);
+            tabla_telefono.TabIndex = 3;
+            tabla_telefono.Text = "telefono";
+            tabla_telefono.UseVisualStyleBackColor = true;
+            // 
+            // tabla_correo
+            // 
+            tabla_correo.Location = new Point(4, 24);
+            tabla_correo.Name = "tabla_correo";
+            tabla_correo.Size = new Size(764, 223);
+            tabla_correo.TabIndex = 4;
+            tabla_correo.Text = "Correo";
+            tabla_correo.UseVisualStyleBackColor = true;
+            // 
+            // tabla_pagina
+            // 
+            tabla_pagina.Location = new Point(4, 24);
+            tabla_pagina.Name = "tabla_pagina";
+            tabla_pagina.Size = new Size(764, 223);
+            tabla_pagina.TabIndex = 5;
+            tabla_pagina.Text = "Pagina Web";
+            tabla_pagina.UseVisualStyleBackColor = true;
+            // 
+            // tabla_comentarios
+            // 
+            tabla_comentarios.Location = new Point(4, 24);
+            tabla_comentarios.Name = "tabla_comentarios";
+            tabla_comentarios.Size = new Size(764, 223);
+            tabla_comentarios.TabIndex = 6;
+            tabla_comentarios.Text = "Comentarios";
+            tabla_comentarios.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(737, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 7;
+            label2.Text = "imagen";
             // 
             // Proveedores
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 192, 192);
             ClientSize = new Size(800, 450);
             Controls.Add(tabControl1);
             Controls.Add(panel1);
@@ -167,7 +239,13 @@
         private Button btn_regresarPC;
         private PictureBox pictureBox1;
         private TabControl tabControl1;
-        private TabPage tabla_numero;
-        private TabPage tabla_nombreEmpresa;
+        private TabPage tabla_empresa;
+        private Button btn_editarProveedor;
+        private TabPage tabla_representante;
+        private TabPage tabla_telefono;
+        private TabPage tabla_correo;
+        private TabPage tabla_pagina;
+        private TabPage tabla_comentarios;
+        private Label label2;
     }
 }
